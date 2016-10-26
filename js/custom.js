05d2b4e4995e36e9b30d8fabba0ebc62af82ad3a
Wun Chiou <wun@utdallas.edu>
2016-10-26 12:03:47 -0500

    Adds a down-click listener to down arrows

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,6 @@ if (typeof jQuery === 'function') {
 
 /**
  * Iterate over an HTMLCollection and perform an action on each element.
- * 
  * @param  {HTMLCollection} collection
  * @param  {function} action
  */
@@ -24,7 +23,6 @@ function iterateCollection(collection, action) {
 
 /**
  * Add 'show' class to an element.
- * 
  * @param  {HTMLElement} element
  */
 function addShowClass(element) {
@@ -33,19 +31,34 @@ function addShowClass(element) {
 
 /**
  * Removes 'show' class from an element.
- * 
  * @param  {HTMLElement} element
  */
 function removeShowClass(element) {
   element.className = element.className.replace(/ \bshow/g, '');
 }
 
+/**
+ * Adds a down click action to an element.
+ * @param  {HTMLElement} element
+ */
+function downClick(element) {
+  element.addEventListener('click', function() {
+    Reveal.down();
+  });
+}
+
+/** @type {string} The name of the class for the clickable down indicator elements */
+var down_click_class = 'down-slide-link';
+
 // When a slide has an available down route, show appearable child elements for that slide.
 // Otherwise, hide all appearable elements in the presentation.
 Reveal.addEventListener( 'slidechanged', function( event ) {
   if (Reveal.availableRoutes().down) {
-    iterateCollection(Reveal.getCurrentSlide().getElementsByClassName('appear'), addShowClass);
+    iterateCollection(Reveal.getCurrentSlide().getElementsByClassName(down_click_class), addShowClass);
   } else {
-    iterateCollection(document.getElementsByClassName('appear'), removeShowClass);
+    iterateCollection(document.getElementsByClassName(down_click_class), removeShowClass);
   }
 });
+
+// When the user clicks on down indicator elements, go down.
+iterateCollection(document.getElementsByClassName(down_click_class), downClick);
